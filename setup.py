6263313a7ca4a6e7f4589ffd5a520ccaede90d80
Anthony Long <antlong@gmail.com>
2011-04-07 15:06:09 -0400

    Automatic commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='assertlib',
+    version='0.1',
+    requires=['functools'],
+    install_requires=['distribute'],
+    author='Anthony Long',
+    author_email='antlong@gmail.com',
+    url='http://www.antlong.com/',
+    packages=['assertlib'],
+)
