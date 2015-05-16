4a6849697ac859a33f67ba254a9a558b60564fce
Anthony Long <antlong@gmail.com>
2015-05-16 06:09:41 -0400

    Fixed some things

--- assertlib/assertlib.py
+++ assertlib/assertlib.py
@@ -1,135 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""assertlib is a library of standalone assertion methods.
+
+  Examples:
+    >>> import assertlib
+    >>> assertlib.assertEither("a", "y", "a")
+"""
 import itertools
+from types import StringType
 
 
 def assertEqual(x, y):
-    """
-    This method asserts that an object is equal to a control.
+    """Asserts that an object is equal to another.
 
-    >>> assertEqual(1, 1)
-    >>> assertEqual(str(1), '1')
-    """
-    if not x == y:
-        raise AssertionError("%s is not equal to %s" % (x, y))
-
-def assertPrecision(object, amount):
-    """
-    Asserts an object has x amount of precision.
-
-    >>> assertPrecision(1.111, 3)
-    """
-    if not len(str(format(object)).partition('.')[2]) == int(amount):
-        raise AssertionError("%s does not have %s precision." % (str(object), str(amount)))
-
-
-def assertEquals(x, y):
-    """
-    This method asserts that x is equal to y.
-    
-    >>> assertEquals(1, 1)
-    >>> assertEquals(str(1), '1')
-    """
-    assertEqual(x, y)
-
-
-def assertNotEqual(x, y):
-    """
-    This method asserts that x is not equal to y.
-    
-    >>> assertNotEqual(1, '1')
-    >>> assertNotEqual(int("1"), '1')
+    Examples:
+      >>> assertEqual(1, 1)
+      >>> assertEqual(str(1), '1')
     """
     if x == y:
-        raise AssertionError("%s did not equal %s." % (x, y))
+        return
+    else:
+        raise AssertionError("%s is not equal to %s" % (x, y))
 
 
-def assertNotEquals(object, control):
+assertEquals = assertEqual
+
+
+def assertPrecision(a, amount):
+    """Asserts that an object has x amount of precision.
+
+    Examples:
+      >>> assertPrecision(1.111, 3)
     """
-    This method asserts that an object is not equal to a control.
-    
-    >>> assertNotEquals(1, '1')
-    >>> assertNotEquals(int("1"), '1')
+    if not len(str(format(a)).partition('.')[2]) == int(amount):
+        raise AssertionError("{a} does not have {amount} precision.".format(
+            str(a), str(amount))
+    )
+
+
+def assertNotEqual(a, b):
+    """This method asserts that x is not equal to y.
+
+    Examples:
+      >>> assertNotEqual(1, '1')
+      >>> assertNotEqual(int("1"), '1')
     """
-    assertNotEqual(object, control)
+    if a == b:
+        raise AssertionError("%s did not equal %s." % (a, b))
 
 
-def assertTrue(object):
+assertNotEquals = assertNotEqual
+
+
+def assertTrue(x):
+    """Asserts that an object or expression evaluates to True.
+
+    Examples:
+      >>> assertTrue(1)
     """
-    This method asserts that an object or expression evaluates to True.
-    
-    >>> assertTrue(1)
-    """
-    if not bool(object):
-        raise AssertionError("%s did not evaluate to True." % object)
+    if not bool(x):
+        raise AssertionError("{x} did not evaluate to True.".format(x))
 
 
-def assertFalse(object):
+def assertFalse(x):
     """
     This method asserts that an object or expression evaluates to False.
 
-    >>> assertFalse(False)
+    Examples:
+      >>> assertFalse(False)
     """
-    if bool(object):
-        raise AssertionError("%s did not evaluate to False." % object)
+    if bool(x):
+        raise AssertionError("%s did not evaluate to False." % x)
 
 
-def assertIs(object, control):
+def assertIs(a, b):
     """
     This method asserts that an object evaluates to a control.
 
-    >>> def x():
-    ...     return 1
-    >>>
-    >>> y = x()
-    >>> z = x()
-    >>> assertIs(y, z)
-    >>> assertIs(1, y)
-    >>> assertIs(1, z)
+    Examples:
+      >>> def x():
+      ...     return 1
+      >>>
+      >>> y = x()
+      >>> z = x()
+      >>> assertIs( y, z )
+      >>> assertIs( 1, y )
+      >>> assertIs( 1, z )
     """
-    if not object is control:
-        raise AssertionError("%s is not %s" % (object, control))
+    if a is not b:
+        raise AssertionError("{a} is not {b}".format(a, b))
 
 
-def assertIsInstance(object, type):
-    if not isinstance(object, type):
-        raise AssertionError("%s is not an instance of %s" % (object, type))
+def assertIsNot(a, b):
+    """Asserts that a is not b
 
-
-def assertIsNotInstance(object, type):
-    if isinstance(object, type):
-        raise AssertionError("%s is an instance of %s" % (object, type))
-
-
-def assertIsNot(object, control):
-    if object is control:
-        raise AssertionError("%s is %s" % (object, control))
-
-
-def assertAlmostEqual(first, second, places=None, epsilon=None):
+    Examples:
+      >>> assertIsNot(1, 2)
     """
+    if a is b:
+        raise AssertionError("{a} is {b}".format(a, b))
+
+
+def assertIsInstance(x, instanceof):
+    """Assert an object is an instance of a type.
+
+    Examples:
+      >>> assertIsInstance("foo", StringType)
+    """
+    if not isinstance(x, instanceof):
+        raise AssertionError("{} is not an instance of {}".format(x, instanceof))
+
+
+def assertIsNotInstance(x, instanceof):
+    """Asserts that an object is not an instance of a type.
+
+    Examples:
+      >>> assertIsNotInstance("x", int)
+    """
+    if isinstance(x, instanceof):
+        raise AssertionError("{x} is an instance of {instanceof}".format(x, instanceof))
+
+
+def assertAlmostEqual(a, b, places=None, epsilon=None):
+    """Assert two things are almost equal.
+
     This method will assert that two objects are almost equal.\
     You can use either places or epsilon as an arg, but you can't\
     use both. `When using epsilon, be aware of \
     <http://docs.python.org/tutorial/floatingpoint.html>`_.
-    
-    >>> assertAlmostEqual(1.1, 1.111, places=2)
-    >>> assertAlmostEqual(1.1, 1.11, epsilon=0.01)
-    
+
+    Examples:
+      >>> assertAlmostEqual(1.1, 1.111, places=2)
+      >>> assertAlmostEqual(1.1, 1.11, epsilon=0.01)
     """
-    if first == second:
+    if a == b:
         return
     if places and epsilon:
         raise TypeError("specify delta or places not both")
     if epsilon is not None:
-        if abs(first - second) <= epsilon:
+        if abs(a - b) <= epsilon:
             raise AssertionError(
-            '%s != %s within %s delta' % (first, second, epsilon))
+                '%s != %s within %s delta' % (a, b, epsilon))
     else:
-        if round(abs(second - first), places) == 0:
+        if round(abs(b - a), places) == 0:
             raise AssertionError(
-            '%s != %s within %s places' % (first, second, places))
+                '%s != %s within %s places' % (a, b, places))
 
 
-def assertNotAlmostEqual(first, second, places=None, epsilon=None):
+def assertNotAlmostEqual(a, b, places=None, epsilon=None):
     """
     This method will assert that two objects are not almost equal.\
     You can use either places or epsilon as an arg, but you can't\
@@ -140,23 +158,27 @@ def assertNotAlmostEqual(first, second, places=None, epsilon=None):
     >>> assertNotAlmostEqual(1.1, 1.11, epsilon=5)
     
     """
-    if first != second:
+    if a != b:
         return
-    if first == second:
-        raise AssertionError('%s == %s' % (first, second))
+    if a == b:
+        raise AssertionError('%s == %s' % (a, b))
     if places and epsilon:
         raise TypeError("specify delta or places not both")
     if epsilon is not None:
-        if abs(first - second) >= epsilon:
+        if abs(a - b) >= epsilon:
             raise AssertionError(
-            '%s == %s within %s delta' % (first, second, epsilon))
+                '%s == %s within %s delta' % (a, b, epsilon))
     else:
-        if round(abs(second - first), places) != 0:
+        if round(abs(b - a), places) != 0:
             raise AssertionError(
-            '%s == %s within %s places' % (first, second, places))
+                '%s == %s within %s places' % (a, b, places))
 
 
 def assertSequenceEqual(seq1, seq2, assert_seq_types=False):
+    """Assers that a sequence is equal.
+
+      >>> assertSequenceEqual([1,2,3], [1,2,3])
+    """
     if assert_seq_types and type(seq1) != type(seq2):
         raise TypeError("type %s != type %s" % (type(seq1), type(seq2)))
     if len(seq1) != len(seq2):
@@ -165,22 +187,47 @@ def assertSequenceEqual(seq1, seq2, assert_seq_types=False):
         raise AssertionError("%s is not equal to %s" % (seq1, seq2))
 
 
-def assertSequenceNotEqual(seq1, seq2, assert_seq_types=True):
+def assertSequenceNotEqual(seq1, seq2, assert_seq_types=False):
+    """Asserts that a swquence is not equal.
+
+    Example:
+      >>> assertSequenceNotEqual([1,2,3], [1,2,4])
+    """
     if assert_seq_types and type(seq1) == type(seq2):
         raise TypeError("type %s == type %s" % (type(seq1), type(seq2)))
     if all(a != b for a, b in itertools.izip(seq1, seq2)):
         raise AssertionError("%s is equal to %s" % (seq1, seq2))
 
-def assertEither(control, x, y):
-    """Assert that x evaluates to either y or z."""
-    if control != this or control != z:
-        raise AssertionError("{0} is neither {1} or {2}".format(control,x,y))
 
-def assertNotEither(control, x, y):
-    """Assert that x evaluates to either y or z."""
-    if control == this or control == z:
-        raise AssertionError("{0} is neither {1} or {2}".format(control,x,y))
+def assertEither(a, x, y):
+    """Assert that a evaluates to either y or z.
+
+    Examples:
+      >>> a = 11
+      >>> assertEither(a, 10, 11)
+      >>> assertEither(str(a), "foo", "11")
+      >>> x = ["foo"]
+      >>> assertEither(x, ["bar"], ["foo"])
+    """
+    if a == x or a == y or a in x or a in y:
+        return
+    else:
+        raise AssertionError("{0} is neither {1} or {2}".format(a, x, y))
+
+
+def assertNotEither(a, x, y):
+    """Assert that x does not evaluate to either y or z.
+
+    Examples:
+      >>> a = "A"
+      >>> assertNotEither("a", "c", a)
+      >>> assertNotEither("a", a, "1")
+    """
+    if a == x or a == y or a in x or a in y:
+        raise AssertionError("{} is {} or {}".format(a, x, y))
+
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
--- setup.py
+++ setup.py
@@ -1,12 +1,51 @@
-import setuptools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
 
-setuptools.setup(
+setup(
     name='assertlib',
-    version='0.2',
-    requires=['functools'],
-    install_requires=['distribute'],
+    version='0.3',
+    description='assertlib is a standalone library of assertion methods.',
+    keywords='assert assertions validation valid data test testing \
+        tests unittest pytest nose',
     author='Anthony Long',
     author_email='antlong@gmail.com',
-    url='http://www.antlong.com/',
+    requires=['itertools'],
     packages=['assertlib'],
+    license='Apache 2.0',
+    url='http://github.com/antlong/assertlib',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Customer Service",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Legal Industry",
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Other Audience",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.3",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: SQL",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: Debuggers",
+        "Topic :: Software Development :: Localization",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX']
 )
